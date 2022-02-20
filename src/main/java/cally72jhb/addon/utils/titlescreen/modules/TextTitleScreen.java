b7f9967092b2184aa1c7b05ab55b9dcf28f6c427
cally72jhb <94923723+cally72jhb@users.noreply.github.com>
2022-02-20 18:41:48 +0100

    Delete TextTitleScreen.java

--- src/main/java/cally72jhb/addon/utils/titlescreen/modules/TextTitleScreen.java
+++
@@ -1,9 +0,0 @@
-package cally72jhb.addon.utils.titlescreen.modules;
-
-import cally72jhb.addon.utils.titlescreen.CustomTitleScreen;
-
-public class TextTitleScreen extends PressableElement {
-    public TextTitleScreen(CustomTitleScreen hud, String name, Action action) {
-        super(hud, name, action, true);
-    }
-}
